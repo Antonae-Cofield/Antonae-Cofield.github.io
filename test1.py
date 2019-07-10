cea350b312df460dad2a3af3cd45b6ea46359cdc
Naee <antonae08@schk12.us>
2019-07-10 09:53:46 -0500

    initial commit

---
+++ test1.py
@@ -0,0 +1,25 @@
+friends=["Alyssa","ava","Chine","Frida","Deja","Shania","Deja"]
+print(friends)
+friends.reverse()
+friends2=friends.copy()
+print(friends2)
+print(friends.index("ava"))
+print(friends.count("Deja"))
+friends.sort()
+print(friends)
+#friends.pop("Ninah")
+friends.append("alex")
+friends.insert(2, "Chine")
+friends.remove("Chine")
+friends[0]="Naee"
+#friends.clear
+lucky_numbers=[3,5,7,12,2,16,37]
+print(friends)
+friends.extend(lucky_numbers)
+print(friends)
+print(len(lucky_numbers))
+numbers = [1,2,3,2,1]
+for num in numbers:
+    print(num)
+for i in range(len(numbers)):
+    print(numbers[1])
